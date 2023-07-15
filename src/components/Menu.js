975f0c10969c28f75daa5721ab2f38d74d7b0775
unknown <javiercubas2003@gmail.com>
2023-07-15 12:43:20 +0200

    update

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,34 +6,14 @@ const Menu = () => {
         <>
             <div className="contact">
                 <h3 className="header-menu">VENTAS</h3>
-                <h4 className="content-menu">ATENCIÓN COMERCIAL: <a href="tel:+34919391736">919391736</a></h4>
+                <h4 className="content-menu">ATENCIÓN COMERCIAL: <a href="tel:+34919391736">919 391 736</a></h4>
                 <h3 className="header-menu">LOGÍSTICA</h3>
                 <a href='https://www.google.es/maps/dir//MATCENTER+EUROPE+S.L.U.,+Cam.+de+Humanes+a+Torrej%C3%B3n+de+Velasco,+6,+28991+Torrej%C3%B3n+de+la+Calzada,+Madrid/@40.2079961,-3.8642629,12z/data=!4m9!4m8!1m0!1m5!1m1!1s0xd4190485fc6e097:0x95dd97c3121f41f9!2m2!1d-3.7939569!2d40.2080505!3e0?hl=es'><h4 className="content-menu">CALLE REAL 1-A <br></br> TORREJÓN DE LA CALZADA <br></br> (29991) MADRID <br></br> (junto a los desguaces)</h4></a>
             </div>
             <a href='https://www.google.es/maps/dir//MATCENTER+EUROPE+S.L.U.,+Cam.+de+Humanes+a+Torrej%C3%B3n+de+Velasco,+6,+28991+Torrej%C3%B3n+de+la+Calzada,+Madrid/@40.2079961,-3.8642629,12z/data=!4m9!4m8!1m0!1m5!1m1!1s0xd4190485fc6e097:0x95dd97c3121f41f9!2m2!1d-3.7939569!2d40.2080505!3e0?hl=es'><div className="mapa"></div></a>
             <div className='familias'>
-            <img src='./assets/Pantalla almacén_page-0001.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0002.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0003.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0004.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0005.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0006.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0007.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0008.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0009.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0010.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0011.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0012.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0013.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0014.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0015.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0016.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0017.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0018.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0019.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0020.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0021.jpg' alt='familia' className='familia'/>
-            <img src='./assets/Pantalla almacén_page-0022.jpg' alt='familia' className='familia'/>
+            <img src='./assets/Pantalla almacén_page-0099.jpg' alt='familia' className='familia'/>
+            <img src='./assets/Pantalla almacén_page-0100.jpg' alt='familia' className='familia'/>
             </div>
         </>
     )
