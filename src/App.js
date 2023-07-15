0b8d85179b1472c2ad805631dee048bcb0363057
unknown <javiercubas2003@gmail.com>
2023-07-15 12:38:33 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Menu from './components/Menu';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Menu/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo" />
+        </div>
+    )
+}
+
+export default Header
