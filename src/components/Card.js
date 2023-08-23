94562a76531151e75786ed1b945f68bc4dd60c88
unknown <javiercubas2003@gmail.com>
2023-08-23 13:03:38 +0200

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,13 @@ import React from 'react'
 import './Card.css'
 
 const Card = (props) => {
-  const { image, name, price, url, iva } = props;
+  const { image, name, price, url, pack, iva } = props;
   return (
     <a href={url} className="card-box">
       <div className="foto-card" style={{ backgroundImage: `url(${image})` }} />
       <div className="inside-box">
         <h3 className="title-card">{name}</h3>
-        <div className="precio-card">{price} €{iva == 1? ' + IVA' : '/ud (IVA INCLUIDO)'}</div>
+        <div className="precio-card">{price} €{pack==1? '' : '/ud' + iva == 1? ' + IVA' : ' (IVA INCLUIDO)'}</div>
       </div>
       <button className="cta-card">COMPRA AHORA</button>
     </a>
