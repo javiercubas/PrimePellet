96aeda7cc9c4f4d6d41b7939cdced652122ebf3e
unknown <javiercubas2003@gmail.com>
2023-08-24 12:17:08 +0200

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,15 @@ import './Card.css'
 
 const Card = (props) => {
   const { image, name, price, url, pack, iva } = props;
+
+  // Limitar el título a 75 caracteres y agregar "..." si es más largo
+  const limitedTitle = name.length > 75 ? name.substring(0, 72) + '...' : name;
+
   return (
     <a href={url} className="card-box">
       <div className="foto-card" style={{ backgroundImage: `url(${image})` }} />
       <div className="inside-box">
-        <h3 className="title-card">{name}</h3>
+        <h3 className="title-card">{limitedTitle}</h3>
         <div className="precio-card">{price} €{pack==1? '' : '/ud' + iva == 1? ' + IVA' : ' (IVA INCLUIDO)'}</div>
       </div>
       <button className="cta-card">COMPRA AHORA</button>
